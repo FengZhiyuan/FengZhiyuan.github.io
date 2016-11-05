93755a7b7b3319dc8bbb5c445bf4f3954c950d64
Feng <fengzy21@gmail.com>
2016-11-05 21:48:51 +0800

    留言板添加完毕

--- PersonalWeb.js
+++ PersonalWeb.js
@@ -232,7 +232,319 @@ var bindEventNav = function() {
             position.append(projects())
         })
         // 留言板
+        $('.btn-messageBoard').on('click', function(event) {
+            // 留言板
+            log('click mb')
+            var messageBoard = function(position) {
+                // 当前时间
+                var currentTime = function() {
+                    var d = new Date()
+                    var month = d.getMonth() + 1
+                    var date = d.getDate()
+                    var hours = d.getHours()
+                    var minutes = d.getMinutes()
+                    var seconds = d.getSeconds()
+                    var timeString = `${month}月${date}日`
+                    return timeString
+                }
 
+                // 选择头像
+                var bindEventImg = function() {
+                   $('.img-chose').on('click', function(event) {
+                       var target = $(event.target)
+                       $('.img-chose').removeClass('active-img')
+                       target.addClass('active-img')
+                   })
+                }
+
+                // 统计字数
+                var bindEventCount = function() {
+                    $('textarea').on('input', function(event) {
+                        var length = document.querySelector('textarea').textLength
+                        var remain = 140 - length
+                        if (remain >= 0) {
+                            $('.text-count').text(`${remain}`)
+                        }else {
+                            var t =`
+                            <div class="div-submit">
+                                <span class="vertical-center">已超过<span class="text-count over">${-remain}</span>个字</span>
+                                <button type="button" name="button" class=" btn-submit">提交</button>
+                            </div>
+                            `
+                        $('.div-submit').remove()
+                        $('.div-submit-container').append(t)
+                        }
+                    })
+                }
+
+                // 提交按钮
+                var bindEventSubmit = function() {
+                    $('.btn-submit').on('click', function(event) {
+                        var inputText = $('textarea').val()
+                        if (inputText !== '') {
+                            var name = $('.input-name').val()
+                            if (name === '') {
+                                name = '匿名'
+                            }
+                            var img = $('.active-img').attr('src')
+                            var time = currentTime()
+                            $('.div-insert-container').append(inputInsert(name, img, inputText, time))
+                            $('textarea').val('')
+                            $('.input-name').val('')
+                            $('textarea').blur()
+                            $('.text-count').text(140)
+                            $('.img-chose').removeClass('active-img')
+                            $($('.img-chose')[0]).addClass('active-img')
+                        }
+                    })
+                }
+
+                // 插入新发言
+                var inputInsert = function(name, img, inputText, time) {
+                    var t = `
+                        <div class="div-insert">
+                            <div class="div-img">
+                                <img width=50 height=50 src="${img}" alt="头像" />
+                            </div>
+                            <div class="div-comments">
+                                <p class="p-comment-name">${name}</p>
+                                <p class="p-comment-text">${inputText}</p>
+                                <div class="div-comment-time">${time}</div>
+                            </div>
+                        </div>
+                    `
+                    return t
+                }
+
+                // 建立页面
+                var appendMB = function(position) {
+                    var main = function() {
+                        var m = `
+                        <div class="div-container">
+                            <div class="div-name">
+                                <div class="div-input-name">
+                                    <input class="input-name" type="input" placeholder="请输入昵称" name="name" value="">
+                                </div>
+                                <div class="div-name-img">
+                                    <img class="img-chose active-img" src="http://www.jf258.com/uploads/2015-05-14/190205140.jpg" alt="" />
+                                    <img class="img-chose" src="http://picview01.baomihua.com/photos/20120708/m_14_634773404838750000_37844128.jpg" alt="" />
+                                    <img class="img-chose" src="http://img4.duitang.com/uploads/blog/201508/23/20150823180803_PXcW2.thumb.700_0.jpeg" alt="" />
+                                    <img class="img-chose" src="http://www.duoziwang.com/2016/10/02/1453262447.jpg" alt="" />
+                                    <img class="img-chose" src="http://img4.duitang.com/uploads/item/201601/15/20160115155755_irumV.jpeg" alt="" />
+                                    <img class="img-chose" src="http://img5.duitang.com/uploads/item/201503/14/20150314210331_sNT8z.jpeg" alt="" />
+                                    <img class="img-chose" src="http://img.wxcha.com/file/201605/14/0841295de4.jpg" alt="" />
+                                    <img class="img-chose" src="http://img4.duitang.com/uploads/blog/201508/23/20150823180803_PXcW2.thumb.700_0.jpeg" alt="" />
+                                </div>
+                            </div>
+                            <div class="div-text">
+                                <textarea name="name" rows="4" cols="40"></textarea>
+                            </div>
+                            <div class="div-submit-container">
+                                <div class="div-submit">
+                                    <span class="vertical-center">还能输入<span class="text-count">140</span>个字</span>
+                                    <button type="button" name="button" class=" btn-submit">提交</button>
+                                </div>
+                            </div>
+                            <hr>
+                            <div class="div-insert-container">
+                                <!-- insert -->
+                                <div class="div-insert">
+                                    <div class="div-img">
+                                        <img width=50 height=50 src="http://www.xueshiboke.com/zb_users/upload/2016/08/201608131471067436568880.jpg" alt="头像" />
+                                    </div>
+                                    <div class="div-comments">
+                                        <p class="p-comment-name">林则徐</p>
+                                        <p class="p-comment-text">专注禁烟100年</p>
+                                        <div class="div-comment-time">10月21日</div>
+                                    </div>
+                                </div>
+                                <!-- insert -->
+                            </div>
+                        </div>
+                        `
+                        return m
+                    }
+                    $(position).append(main())
+                }
+
+                //加入CSS
+                var appendCSS = function(position) {
+                    var css = function() {
+                        var c = `
+                        <style>
+                        .vertical-center {
+                            position: absolute;
+                            top: 50%;
+                            transform: translateY(-50%);
+                        }
+
+                        .div-container {
+                            background-color: white;
+                            width: 100%;
+                            padding: 20px 20px;
+                            border-radius: 5px;
+                        }
+
+                        .div-name {
+                            position: relative;
+                            width: 100%;
+                            height: 60px;
+                        }
+
+                        .div-input-name {
+                            display: block;
+                        }
+
+                        .input-name {
+                            margin: 5px;
+                            border-radius: 4px;
+                            border: 1px solid lightgray;
+                            text-align: center;
+                        }
+
+                        .div-name-img {
+                            display: block;
+                            margin: 4px auto 4px 5px;
+                        }
+
+                        .div-name-img img {
+                            display: inline-block;
+                            width: 20px;
+                            height: 20px;
+                        }
+
+                        .active-img {
+                            border: 1px solid black;
+                        }
+
+                        textarea {
+                            width: 98%;
+                            height: 68px;
+                            margin: 5px;
+                            padding: 0;
+                            border-radius: 4px;
+                            border: 1px lightgray solid;
+                        }
+
+                        .div-submit {
+                            position: relative;
+                            width: 98%;
+                            height: 30px;
+                            margin: 0 5px;
+                            text-align: right;
+                            padding: 0;
+                        }
+
+                        .div-submit > span {
+                            color: gray;
+                            height: auto;
+                            font-size: 80%;
+                            right: 125px;
+                        }
+
+                        .text-count {
+                            font-size: 150%;
+                        }
+
+                        .over {
+                            color: red;
+                        }
+
+                        .btn-submit {
+                            width: 120px;
+                            height: 26px;
+                            margin: 2px 0;
+                            border: 1px solid orange;
+                            background-color: orange;
+                            color: white;
+                            border-radius: 4px;
+                            font-weight: 900;
+                            font-size: 110%;
+                        }
+
+                        hr {
+                            border: 0.5px #fff inset;
+                        }
+
+                        .div-insert-container {
+                            position: relative;
+                            width: 600px;
+                        }
+
+                        .div-insert {
+                            border-bottom: 1px dashed gray;
+                            margin: 5px 0;
+                        }
+
+                        .div-img {
+                            display: inline-block;
+                            vertical-align: top;
+                            margin: 5px;
+                            padding: 2px;
+                            padding-right: 0;
+                            margin-right: 0;
+                        }
+
+                        img {
+                            border: 1px solid lightgray;
+                            border-radius: 3px;
+                        }
+
+                        .div-comments {
+                            display: inline-block;
+                            text-align: left;
+                            margin-left: 2px;
+                            width: 525px;
+                            margin: 3px;
+                            font-size: 85%;
+                        }
+
+                        .p-comment-name {
+                            margin-bottom: 5px;
+                            overflow: hidden;
+                            padding: 2px;
+                            overflow: hidden;
+                            word-wrap: break-word;
+                            font-size: 105%;
+                            margin-top: 0;
+                            font-weight: 800;
+
+                        }
+
+                        .p-comment-text {
+                            margin-top: 0px;
+                            font-size: 95%;
+                        }
+
+                        .div-comment-time {
+                            color: gray;
+                            font-size: 80%;
+                            margin-top: 10px;
+                        }
+                        </style>
+                        `
+                        return c
+                    }
+                    $(position).append(css())
+                }
+
+                // Events
+                var bindEvents = function(position) {
+                    appendMB(position)
+                    appendCSS(position)
+                    bindEventSubmit()
+                    bindEventCount()
+                    bindEventImg()
+                }
+
+                bindEvents(position)
+            }
+            var position = $('.div-anchor')
+            $('.nav-btn').removeClass('active')
+            $('.btn-messageBoard').addClass('active')
+            position.empty()
+            messageBoard('.div-anchor')
+        })
     }
 
     bindEventHome()
@@ -258,6 +570,7 @@ var bindEventAside = function() {
             $('.btn-project').click()
         })
     }
+
 
     bindEventActive()
     bindEventIntro()
