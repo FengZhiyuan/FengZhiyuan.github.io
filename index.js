7727a826ea798d3bfb77e249ece0ff94232fd925
Feng <fengzy21@hotmail.com>
2024-07-11 21:57:48 +0800

    update sa-toolw

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const insertDivs = (container, rowIndex) => {
     for (let i = 0; i < 5; i++) {
         const div = document.createElement('span')
         div.classList.add('cell')
-        // div.classList.add('cell-' + rowIndex + '-' + i + 1)
+        div.innerHTML= '<span class="span-jing"></span><span class="span-shuxing"></span>'
         container.appendChild(div)
     }
 }
@@ -39,6 +39,8 @@ const bindEventCell = () => {
                 <div class="item item-shui">水</div>
                 <div class="item item-huo">火</div>
                 <div class="item item-feng">风</div>
+                <div class="item item-jing">镜</div>
+                <div class="item item-kong">空</div>
             `
             target.appendChild(dialog)
             dialog.addEventListener('click', (event) => {
@@ -46,23 +48,33 @@ const bindEventCell = () => {
                 if (target.classList.contains('item')) {
                     const item = target
                     const text = item.innerText
-                    const cell = item.parentElement.parentElement
-                    cell.innerText = text
+                    const cell = item.parentElement.parentElement.parentElement
+
                     // cell 的class 改为选项对应的样式
-                    const classList = cell.classList
-                    classList.remove('item-di')
-                    classList.remove('item-shui')
-                    classList.remove('item-huo')
-                    classList.remove('item-feng')
-                    if (text === '地') {
-                        classList.add('item-di')
-                    } else if (text === '水') {
-                        classList.add('item-shui')
-                    } else if (text === '火') {
-                        classList.add('item-huo')
-                    } else if (text === '风') {
-                        classList.add('item-feng')
+                    if (text === "镜") {
+                        const el = cell.children[0]
+                        const classList = el.classList
+                        classList.toggle('item-jing-show')
+                    //
+                    } else {
+                        const el = cell.children[1]
+                        el.innerText = text
+                        //
+                        const classList = el.classList
+                        classList.remove('item-di', 'item-shui', 'item-huo', 'item-feng')
+                        if (text === '地') {
+                            classList.add('item-di')
+                        } else if (text === '水') {
+                            classList.add('item-shui')
+                        } else if (text === '火') {
+                            classList.add('item-huo')
+                        } else if (text === '风') {
+                            classList.add('item-feng')
+                        } else if (text === '空') {
+                            el.innerText = ""
+                        }
                     }
+
 
                     const dialog = item.parentElement
                     dialog.remove()
@@ -72,13 +84,27 @@ const bindEventCell = () => {
     }
 }
 
+// 点击id="button-refresh"的按钮，清空row1、row2 内的所有内容，重新初始化
+const bindEventRefresh = () => {
+    const button = document.querySelector('#button-refresh')
+    button.addEventListener('click', (event) => {
+        const row1 = document.querySelector("#row1")
+        const row2 = document.querySelector("#row2")
+        row1.innerHTML = ''
+        row2.innerHTML = ''
+        insertDivs(row1, '1')
+        insertDivs(row2, '2')
+        bindEventCell()
+    })
+}
+
 const __main = () => {
-    log('sa-tools')
     const row1 = document.querySelector("#row1")
     const row2 = document.querySelector("#row2")
     insertDivs(row1, '1')
     insertDivs(row2, '2')
     bindEventCell()
+    bindEventRefresh()
 }
 
 
